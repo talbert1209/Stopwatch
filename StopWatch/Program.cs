ade021c53c5f559c90ccd0536e09c7803b618001
talbert1209 <talbert1209@gmail.com>
2018-09-07 09:53:59 -0500

    Add project files.

---
+++ StopWatch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            var stopWatch = new StopWatch();
+
+            Console.WriteLine(@"Enter 'start' to start the watch, 'stop' to stop the watch, 'clear' to clear the watch and 'end' to end the program");
+
+            while (stopWatch.IsStopWatchOn())
+            {
+                var userInput = Console.ReadLine()?.ToLower();
+
+                switch (userInput)
+                {
+                    case "start":
+                        stopWatch.StartTheWatch();
+                        break;
+                    case "stop":
+                        stopWatch.StopTheWatch();
+                        break;
+                    case "clear":
+                        stopWatch.ClearTheWatch();
+                        break;
+                    case "end":
+                        stopWatch.End();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid command!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private TimeSpan _totalDuration;
+        private bool _watchStarted;
+        private bool _watchOn = true;
+
+        public bool IsStopWatchOn()
+        {
+            return _watchOn;
+        }
+
+        public void StartTheWatch()
+        {
+            if (!_watchStarted)
+            {
+                _watchStarted = true;
+                _startTime = DateTime.Now;
+                Console.WriteLine("Watch started!");
+            }
+            else
+            {
+                Console.WriteLine("The watch is already started!");
+            }
+        }
+
+        public void StopTheWatch()
+        {
+            if (_watchStarted)
+            {
+                _watchStarted = false;
+                _endTime = DateTime.Now;
+                var duration = _endTime - _startTime;
+                _totalDuration += duration;
+                Console.WriteLine($"Time Elapsed: {_totalDuration}");
+            }
+            else
+            {
+                Console.WriteLine("The watch is already stopped!");
+            }
+        }
+
+        public void ClearTheWatch()
+        {
+            if (!_watchStarted) 
+            {
+                _totalDuration = TimeSpan.Zero;
+                Console.WriteLine("Stopwatch cleared!");
+            }
+            else
+            {
+                Console.WriteLine("Watch must be stopped to clear!");
+            }
+        }
+
+        public void End()
+        {
+            _watchOn = false;
+        }
+    }
+}
